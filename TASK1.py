46d393866418ea0e950450b8c8aea38fe408d931
shasa21017 <saniashaikh24123@gmail.com>
2025-11-13 01:49:07 -0800

    initail commit

---
+++ TASK1.py
@@ -0,0 +1,6 @@
+Name = input("Enter your name: ")
+Age = input("Enter your age: ")
+Hobby = input("Enter your hobby: ")
+print("\nHello," +Name+"!","\nWelcome to the Python Program.","You're " +Age+" years old and love "
+                                                                            +Hobby+".\nIt's great to know more about you.")
+
